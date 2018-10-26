0a86994daadaebb7edb6354f917b4b76dad5b7d0
Rodrigo Aggeu <rodrigoaggeu@gmail.com>
2018-10-25 21:50:54 -0300

    Update icone e modal Sobre, add item menu config e operador na PrincipalView

--- view/CautelaView.php
+++ view/CautelaView.php
@@ -64,7 +64,7 @@
         <li class="nav-item dropdown no-arrow">
           <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <div class="exit">
-              <i class="material-icons" >settings</i>
+            <i class="material-icons">menu</i>
             </div>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
