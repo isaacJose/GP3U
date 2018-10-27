919f2220e32fae8307b3d5a9d512504669137393
Vanderson Fábio <vanderson.fabio@gmail.com>
2018-10-27 15:44:30 -0300

    Caracteres configurados com sucesso!

--- DAO/Conexao.php
+++ DAO/Conexao.php
@@ -4,6 +4,7 @@ class conexao {
 
     public function conecta() {
         $conn = new mysqli('localhost', 'root', '', 'sigep', '3306');
+        $conn->set_charset("utf8");
         if (!$conn) {
             die('Nao foi possível realizar a conexão ao BD, erro: ' . mysqli_connect_error());
         }
