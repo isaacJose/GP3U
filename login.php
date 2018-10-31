7c1dd634ff5fb4aca39b469d284f1289d0d9b159
Bruno Silva <brunosilv_a@outlook.com>
2018-10-31 16:48:03 -0300

    update in login, register and forgot pages

--- login.php
+++ login.php
@@ -1,33 +1,8 @@
 <?php
-//session_start();
+session_unset();
+session_destroy();
+include 'view\includes\headerpass.html';
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SIGEP</title>
-
-    <!-- Favicon-->
-    <link rel="icon" type="image/png" sizes="16x16" href="img/sigeplogo2.png">
-
-    <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin.css" rel="stylesheet">
-
-  </head>
 
   <body class="bg-dark">
 
